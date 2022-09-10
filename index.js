3db7b4319f0875225c6b21b878ce6bfa28534386
jola-blvhk <awujoolabello@gmail.com>
2022-09-10 01:35:36 +0100

    pushing all

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var sideMenu = document.querySelector(".fa-bars");
 var navLinks = document.querySelector(".nav_links");
-
+var button = document.querySelector(".button");
+var navandbutton = document.querySelector(".navandbutton")
 
    sideMenu.addEventListener("click", () => {
         navLinks.classList.toggle("dropSideMenu");
-        navLinks.classList.toggle("nav_links")
+        navLinks.classList.toggle("nav_links");
+      //   button.classList.toggle("header-button");
+      //   button.classList.toggle("button");
+      //   navandbutton.classList.toggle("navendbutton");
+      //   navandbutton.classList.toggle("navandbutton");
    });    
       
     
